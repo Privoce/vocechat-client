3d90d5bd6757b36055e32b358fcf6bcf5d00736a
shuotwang <shuotongwang@gmail.com>
2022-12-01 21:09:07 -0800

    temp add: check updates feature in about page

--- lib/ui/settings/settings_about_page.dart
+++ lib/ui/settings/settings_about_page.dart
@@ -1,17 +1,24 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:universal_html/js.dart';
+import 'package:vocechat_client/app.dart';
+import 'package:vocechat_client/app_alert_dialog.dart';
 import 'package:vocechat_client/app_text_styles.dart';
 import 'package:vocechat_client/ui/app_colors.dart';
+import 'package:vocechat_client/ui/settings/changelog_models.dart/change_log.dart';
 import 'package:vocechat_client/ui/widgets/app_icon.dart';
 import 'package:vocechat_client/ui/widgets/banner_tile/banner_tile.dart';
 import 'package:vocechat_client/ui/widgets/banner_tile/banner_tile_group.dart';
 import 'package:http/http.dart' as http;
 
 class SettingsAboutPage extends StatelessWidget {
+  final ValueNotifier<bool> _isCheckingUpdates = ValueNotifier(false);
+
   @override
   Widget build(BuildContext context) {
     _getChangeLog();
@@ -30,7 +37,7 @@ class SettingsAboutPage extends StatelessWidget {
             },
             child: Icon(Icons.arrow_back_ios_new, color: AppColors.grey97)),
       ),
-      body: SafeArea(child: _buildBody()),
+      body: SafeArea(child: _buildBody(context)),
       bottomNavigationBar: SafeArea(
           child: Text(
         "© Privoce Inc. All Rights Reserved.",
@@ -39,14 +46,14 @@ class SettingsAboutPage extends StatelessWidget {
     );
   }
 
-  Widget _buildBody() {
+  Widget _buildBody(BuildContext context) {
     return Column(
       children: [
         AppIcon(),
         SizedBox(height: 16),
         _buildAppInfo(),
         SizedBox(height: 32),
-        _buildActions()
+        _buildActions(context)
       ],
     );
   }
@@ -70,9 +77,24 @@ class SettingsAboutPage extends StatelessWidget {
     );
   }
 
-  Widget _buildActions() {
+  Widget _buildActions(BuildContext context) {
     return BannerTileGroup(bannerTileList: [
-      BannerTile(title: "Check Updates"),
+      BannerTile(
+        title: "Check Updates",
+        titleWidget: ValueListenableBuilder<bool>(
+          valueListenable: _isCheckingUpdates,
+          builder: (context, value, child) {
+            if (value) {
+              return CupertinoActivityIndicator();
+            } else {
+              return SizedBox.shrink();
+            }
+          },
+        ),
+        onTap: () {
+          _checkUpdates(context);
+        },
+      ),
       BannerTile(title: "Changelog")
     ]);
   }
@@ -85,9 +107,87 @@ class SettingsAboutPage extends StatelessWidget {
     return version;
   }
 
-  Future<Map<String, dynamic>?> _getChangeLog() async {
-    final logUrl = "https://vocechat.s3.amazonaws.com/changelog.json";
-    final res = await http.get(Uri.parse(logUrl));
-    return jsonDecode(res.body)["latest"];
+  void _checkUpdates(BuildContext context) async {
+    _isCheckingUpdates.value = true;
+
+    final changeLog = await _getChangeLog();
+
+    if (changeLog == null) {
+      _showNetworkError(context);
+      _isCheckingUpdates.value = false;
+      return;
+    }
+
+    final latestVersion = changeLog.latest.version;
+    final localVersion = await _getAppVersion();
+
+    if (latestVersion.compareTo(localVersion) > 0) {
+      _showUpdates(context, latestVersion);
+    } else {
+      _showUpToDate(context);
+    }
+
+    _isCheckingUpdates.value = false;
+  }
+
+  Future<ChangeLog?> _getChangeLog() async {
+    try {
+      const logUrl = "https://vocechat.s3.amazonaws.com/changelog.json";
+      final res = await http.get(Uri.parse(logUrl));
+      return ChangeLog.fromJson(jsonDecode(res.body));
+    } catch (e) {
+      App.logger.severe(e);
+    }
+    return null;
+  }
+
+  void _showNetworkError(BuildContext context) {
+    showAppAlert(
+        context: context,
+        title: "Network Error",
+        content:
+            "VoceChat cannot fetch latest version information. You may try it later.",
+        actions: [
+          AppAlertDialogAction(
+              text: "OK", action: () => Navigator.of(context).pop())
+        ]);
+  }
+
+  void _showUpdates(BuildContext context, String latestVersionNum) {
+    List<AppAlertDialogAction> actions = [];
+    if (Platform.isIOS) {
+      actions.add(AppAlertDialogAction(text: "App Store", action: (() {})));
+    } else if (Platform.isAndroid) {
+      actions.addAll([
+        AppAlertDialogAction(text: "Play Store", action: (() {})),
+        AppAlertDialogAction(text: "APK", action: (() {}))
+      ]);
+    }
+
+    actions.add(AppAlertDialogAction(
+        text: "Cancel",
+        action: (() {
+          Navigator.of(context).pop();
+        })));
+
+    showAppAlert(
+        context: context,
+        title: "Update Available",
+        content: "A newer version $latestVersionNum is available.",
+        actions: actions);
+  }
+
+  void _showUpToDate(BuildContext context) {
+    showAppAlert(
+        context: context,
+        title: "You are Up-to-date",
+        content: "You are using the latest VoceChat App.",
+        actions: [
+          AppAlertDialogAction(
+              text: "OK",
+              action: (() {
+                Navigator.of(context).pop();
+              }))
+        ]);
   }
 }
