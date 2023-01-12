43b0bfde1a5599a67561f7c2af4aea813b31fd31
shuotwang <shuotongwang@gmail.com>
2023-01-12 21:22:50 +0800

    improve FCM issue (related with GFW), and add popup alerts.

--- lib/ui/chats/chat/message_tile/image_bubble.dart
+++ lib/ui/chats/chat/message_tile/image_bubble.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:insta_image_viewer/insta_image_viewer.dart';
 import 'package:vocechat_client/fade_page_route.dart';
 import 'package:vocechat_client/services/send_service.dart';
 import 'package:vocechat_client/ui/chats/chat/image_page.dart';
@@ -35,19 +36,26 @@ class _ImageBubbleState extends State<ImageBubble> {
         child: widget.imageFile == null
             ? TextBubble(
                 content: "Image might have been deleted.", hasMention: false)
-            : GestureDetector(
-                behavior: HitTestBehavior.opaque,
-                child: Hero(
-                    tag: widget.localMid,
-                    child: Image.file(widget.imageFile!, fit: BoxFit.contain)),
-                onTap: () {
-                  Navigator.of(context).push(FadePageRoute(
-                      interBarrierColor: Colors.black,
-                      child: ImagePageTest(
-                          initImageFile: widget.imageFile!,
-                          localMid: widget.localMid,
-                          getImage: widget.getImage)));
-                },
-              ));
+            : InstaImageViewer(
+                child: Image(
+                    image:
+                        // Image.network("https://picsum.photos/id/507/1000").image,
+                        Image.file(widget.imageFile!).image),
+              )
+        // GestureDetector(
+        //     behavior: HitTestBehavior.opaque,
+        //     child: Hero(
+        //         tag: widget.localMid,
+        //         child: Image.file(widget.imageFile!, fit: BoxFit.contain)),
+        //     onTap: () {
+        //       Navigator.of(context).push(FadePageRoute(
+        //           interBarrierColor: Colors.black,
+        //           child: ImagePageTest(
+        //               initImageFile: widget.imageFile!,
+        //               localMid: widget.localMid,
+        //               getImage: widget.getImage)));
+        //     },
+        //   )
+        );
   }
 }
