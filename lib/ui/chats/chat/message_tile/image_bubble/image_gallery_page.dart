b2bb1bdd1715b688ad0c427a0e0606f0b01c78ce
shuotwang <shuotongwang@gmail.com>
2023-01-16 18:28:44 +0800

    delete adding indicators

--- lib/ui/chats/chat/message_tile/image_bubble/image_gallery_page.dart
+++ lib/ui/chats/chat/message_tile/image_bubble/image_gallery_page.dart
@@ -21,15 +21,32 @@ class _ImageGalleryPageState extends State<ImageGalleryPage> {
   final List<SingleImageItem> _imageList = [];
   final Set<String> _imageLocalMidSet = {};
 
-  bool _isAddingPre = false;
-  bool _isAddingNext = false;
-
   @override
   void initState() {
     super.initState();
 
+    _initialize(widget.initImageItem);
+
+    // _loadFrontBackImages(widget.initImageItem);
+  }
+
+  void _initController() {
     _controller = PageController(keepPage: true);
-    _addToEnd(widget.initImageItem);
+
+    _controller.addListener(() {
+      final currentIndex = _controller.page!.round();
+      SingleImageItem currItem = _imageList[_controller.page!.round()];
+      print("listener _currentIndex: $currentIndex");
+      if (currentIndex == 0) {
+        _loadPreImages(currItem);
+      }
+
+      if (currentIndex == _imageList.length) {
+        _loadNextImages(currItem);
+      }
+
+      // setState(() {});
+    });
   }
 
   void _addToFront(SingleImageItem item) {
@@ -37,7 +54,7 @@ class _ImageGalleryPageState extends State<ImageGalleryPage> {
       _imageLocalMidSet.add(item.chatMsgM.localMid);
       _imageList.insert(0, item);
 
-      _imageList.sort(((a, b) => a.chatMsgM.mid.compareTo(b.chatMsgM.mid)));
+      // _imageList.sort(((a, b) => a.chatMsgM.mid.compareTo(b.chatMsgM.mid)));
     }
   }
 
@@ -46,7 +63,7 @@ class _ImageGalleryPageState extends State<ImageGalleryPage> {
       _imageLocalMidSet.add(item.chatMsgM.localMid);
       _imageList.add(item);
 
-      _imageList.sort(((a, b) => a.chatMsgM.mid.compareTo(b.chatMsgM.mid)));
+      // _imageList.sort(((a, b) => a.chatMsgM.mid.compareTo(b.chatMsgM.mid)));
     }
   }
 
@@ -56,75 +73,64 @@ class _ImageGalleryPageState extends State<ImageGalleryPage> {
         color: Colors.black,
         child: PageView.builder(
           controller: _controller,
-          allowImplicitScrolling: true,
+          // allowImplicitScrolling: true,
           itemCount: _imageList.length,
           physics: Platform.isIOS
               ? BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics())
               : AlwaysScrollableScrollPhysics(),
-          itemBuilder: (context, index) {
-            final item = _imageList[index];
-            _addImage(item, index);
-
-            return SingleImagePage(
-                initImageFile: item.initImageFile,
-                loadOriginalImageFileCallBack: ((progressIndicator) {}));
-          },
+          itemBuilder: _buildItem,
         ));
   }
 
-  void _addImage(SingleImageItem currItem, int index) async {
-    for (final each in _imageList) {
-      print(each.chatMsgM.msgNormal?.content);
-    }
-    if (index <= 1) {
-      await _addPreImages(currItem);
-    }
-    if (index >= _imageList.length - 2) {
-      await _addNextImages(currItem);
+  Widget _buildItem(BuildContext context, int index) {
+    final item = _imageList[index];
+    print("builder index $index");
+    // print("length: ${_imageList.length}");
+
+    return SingleImagePage(
+        initImageFile: item.initImageFile,
+        loadOriginalImageFileCallBack: ((progressIndicator) {}));
+  }
+
+  void _initialize(SingleImageItem currItem) async {
+    _initController();
+
+    await _loadPreImages(currItem);
+    await _loadNextImages(currItem);
+  }
+
+  Future<void> _loadPreImages(SingleImageItem currItem) async {
+    final preMsg = await ChatMsgDao().getPreImageMsgBeforeMid(
+        currItem.chatMsgM.mid,
+        uid: currItem.chatMsgM.dmUid,
+        gid: currItem.chatMsgM.gid);
+    if (preMsg != null) {
+      final imageFile = await _getImageFile(preMsg);
+      if (imageFile != null && mounted) {
+        print("currMid: ${currItem.chatMsgM.mid}, preMid: ${preMsg.mid}");
+        setState(() {
+          _addToFront(
+              SingleImageItem(initImageFile: imageFile, chatMsgM: preMsg));
+          // _controller.jumpToPage(_controller.page!.round() + 1);
+          // _currentIndex += 1;
+        });
+      }
     }
   }
 
-  Future<void> _addPreImages(SingleImageItem currItem) async {
-    if (_isAddingPre) {
-      return;
-    } else {
-      _isAddingPre = true;
-      final msg = await ChatMsgDao().getPreImageMsgBeforeMid(
-          currItem.chatMsgM.mid,
-          uid: currItem.chatMsgM.dmUid,
-          gid: currItem.chatMsgM.gid);
-      if (msg != null) {
-        final imageFile = await _getImageFile(msg);
-        if (imageFile != null && mounted) {
-          setState(() {
-            _addToFront(
-                SingleImageItem(initImageFile: imageFile, chatMsgM: msg));
-          });
-        }
+  Future<void> _loadNextImages(SingleImageItem currItem) async {
+    final nextMsg = await ChatMsgDao().getNextImageMsgAfterMid(
+        currItem.chatMsgM.mid,
+        uid: currItem.chatMsgM.dmUid,
+        gid: currItem.chatMsgM.gid);
+    if (nextMsg != null) {
+      final imageFile = await _getImageFile(nextMsg);
+      if (imageFile != null && mounted) {
+        setState(() {
+          _addToEnd(
+              SingleImageItem(initImageFile: imageFile, chatMsgM: nextMsg));
+        });
       }
-
-      _isAddingPre = false;
-    }
-  }
-
-  Future<void> _addNextImages(SingleImageItem currItem) async {
-    if (_isAddingNext) {
-      return;
-    } else {
-      _isAddingNext = true;
-      final msg = await ChatMsgDao().getNextImageMsgAfterMid(
-          currItem.chatMsgM.mid,
-          uid: currItem.chatMsgM.dmUid,
-          gid: currItem.chatMsgM.gid);
-      if (msg != null) {
-        final imageFile = await _getImageFile(msg);
-        if (imageFile != null && mounted) {
-          setState(() {
-            _addToEnd(SingleImageItem(initImageFile: imageFile, chatMsgM: msg));
-          });
-        }
-      }
-      _isAddingNext = false;
     }
   }
 
