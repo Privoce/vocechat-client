33c7fb8132643db6c3265ba89491d9e783386079
shuotwang <shuotongwang@gmail.com>
2023-02-21 09:18:10 +0800

    some package updates

--- lib/main.dart
+++ lib/main.dart
@@ -317,7 +317,7 @@ class _VoceChatAppState extends State<VoceChatApp> with WidgetsBindingObserver {
     print(message.data);
   }
 
-  Future<InvitationLinkData?> _parseInvitationLink(Uri uri) async {
+  Future<InvitationLinkData?> _parseLink(Uri uri) async {
     final param = uri.queryParameters["magic_link"];
     if (param == null || param.isEmpty) return null;
 
@@ -370,7 +370,7 @@ class _VoceChatAppState extends State<VoceChatApp> with WidgetsBindingObserver {
     uriLinkStream.listen((Uri? uri) async {
       if (uri == null) return;
 
-      final linkData = await _parseInvitationLink(uri);
+      final linkData = await _parseLink(uri);
       if (linkData == null) return;
 
       _handleUniLink(linkData);
@@ -392,7 +392,7 @@ class _VoceChatAppState extends State<VoceChatApp> with WidgetsBindingObserver {
     final initialUri = await getInitialUri();
     if (initialUri == null) return;
 
-    final linkData = await _parseInvitationLink(initialUri);
+    final linkData = await _parseLink(initialUri);
     if (linkData == null) return;
 
     _handleUniLink(linkData);
