833747e12bae08f4ecee3a1d5ba829f6de30c9c6
shuotwang <shuotongwang@gmail.com>
2023-05-16 23:04:32 -0700

    release v0.2.87

--- lib/ui/widgets/channel_start.dart
+++ lib/ui/widgets/channel_start.dart
@@ -30,40 +30,44 @@ class ChannelStart extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              RichText(
-                  text: TextSpan(children: [
-                TextSpan(text: "Welcome to ", style: AppTextStyles.titleLarge),
-                TextSpan(text: "#$_title", style: AppTextStyles.titleLarge)
-              ])),
-              SizedBox(height: 8),
-              Text("This is the start of the #$_title channel.",
-                  style: AppTextStyles.snippet),
-              SizedBox(height: 10),
-              if (isOwner || isAdmin)
-                CupertinoButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ChannelInfoPage(groupInfoNotifier)
-                        // ChannelSettingsPage(groupInfoNotifier: groupInfoNotifier),
-                        ));
-                  },
-                  child: Row(
-                    children: const [
-                      Icon(AppIcons.edit, size: 16),
-                      SizedBox(width: 8),
-                      Text(
-                        "Edit Channel",
-                        style: TextStyle(fontSize: 16),
-                      )
-                    ],
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RichText(
+                    text: TextSpan(children: [
+                  TextSpan(
+                      text: "Welcome to ", style: AppTextStyles.titleLarge),
+                  TextSpan(text: "#$_title", style: AppTextStyles.titleLarge)
+                ])),
+                SizedBox(height: 8),
+                Text("This is the start of the #$_title channel.",
+                    style: AppTextStyles.snippet),
+                SizedBox(height: 10),
+                if (isOwner || isAdmin)
+                  CupertinoButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) =>
+                              ChannelInfoPage(groupInfoNotifier)
+                          // ChannelSettingsPage(groupInfoNotifier: groupInfoNotifier),
+                          ));
+                    },
+                    child: Row(
+                      children: const [
+                        Icon(AppIcons.edit, size: 16),
+                        SizedBox(width: 8),
+                        Text(
+                          "Edit Channel",
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              Divider(color: AppColors.grey400),
-            ],
+                Divider(color: AppColors.grey400),
+              ],
+            ),
           ),
           Container(
             width: 24,
