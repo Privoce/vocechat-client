6251ab8ab0af7a816eb10182e0a2d9d55153d1ac
shuotwang <shuotongwang@gmail.com>
2023-06-15 17:06:17 -0700

    basic UI for audio/video chats.

--- lib/ui/agora/av_action_sheet.dart
+++ lib/ui/agora/av_action_sheet.dart
@@ -1,64 +1,164 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:vocechat_client/ui/widgets/sheet_app_bar.dart';
 
 class AvActionSheet extends StatefulWidget {
-  final List<AvActionSheetItem> items;
+  // final List<AvActionSheetBtnItem> btnItems;
+  // final List<AvActionSheetListItem>? listItems;
+  final List<Widget> btnItems;
+  final List<Widget>? listItems;
 
-  const AvActionSheet({Key? key, required this.items}) : super(key: key);
+  const AvActionSheet({Key? key, required this.btnItems, this.listItems})
+      : super(key: key);
 
   @override
   State<AvActionSheet> createState() => _AvActionSheetState();
 }
 
 class _AvActionSheetState extends State<AvActionSheet> {
-  final double _minHeight = 100;
-  final double _maxHeight = 300;
+  final double _dragBarHeight = 16;
+  // final double _actionBtnBarHeight = 64;
+  final double _actionBtnBarTopPadding = 8;
+  final double _actionBtnBarBottomPadding = 8;
+  final double _actionBtnHeight = 56;
+
+  final DraggableScrollableController draggableScrollableController =
+      DraggableScrollableController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     final pageHeight = MediaQuery.of(context).size.height;
-    final initialChildSize = _minHeight / pageHeight;
-    final minChildSize = _minHeight / pageHeight;
-    final maxChildSize = _maxHeight / pageHeight;
+    final bottomOffset = MediaQuery.of(context).padding.bottom;
+
+    final maxHeight = pageHeight * 0.6;
+    final minHeight = _dragBarHeight +
+        _actionBtnBarTopPadding +
+        _actionBtnBarBottomPadding +
+        _actionBtnHeight +
+        bottomOffset;
+    final initialChildSize = minHeight / pageHeight;
+    final minChildSize = minHeight / pageHeight;
+    final maxChildSize = maxHeight / pageHeight;
 
     return DraggableScrollableSheet(
+      // controller: draggableScrollableController,
       initialChildSize: initialChildSize,
       minChildSize: minChildSize,
       maxChildSize: maxChildSize,
+      snap: true,
+      snapSizes: [minChildSize, maxChildSize],
       builder: (context, controller) => Container(
         decoration: BoxDecoration(
-          color: Colors.amber,
+          color: Colors.grey[900],
           borderRadius: BorderRadius.vertical(
-            top: Radius.circular(16.0),
+            top: Radius.circular(_dragBarHeight),
           ),
         ),
-        child: Column(
+        child: Stack(
           children: [
-            SheetAppBar(),
-            Flexible(
-              child: Container(
-                color: Colors.blue,
-                child: ListView.builder(
-                  padding: EdgeInsets.zero,
-                  controller: controller,
-                  itemCount: 50,
-                  itemBuilder: (context, index) => ListTile(
-                    title: Text('Item $index'),
-                  ),
+            ListView(
+              padding: EdgeInsets.only(top: _dragBarHeight),
+              controller: controller,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(
+                      top: _actionBtnBarTopPadding,
+                      bottom: _actionBtnBarBottomPadding,
+                      left: 16,
+                      right: 16),
+                  child: _buildActions(context),
                 ),
-              ),
+                ..._prepareList()
+              ],
             ),
+            IgnorePointer(
+                ignoring: false,
+                child: SizedBox(
+                    width: double.maxFinite,
+                    height: _dragBarHeight,
+                    child: Column(
+                      children: [
+                        SheetAppBar(),
+                      ],
+                    ))),
           ],
         ),
       ),
     );
   }
+
+  List<Widget> _prepareList() {
+    if (widget.listItems == null) return List.empty();
+
+    return widget.listItems!;
+  }
+
+  Widget _buildActions(
+    BuildContext context,
+  ) {
+    const double minGap = 16.0;
+    const double buttonIconSize = 24;
+    const double buttonSize = 56; // 24 + 16*2
+
+    return LayoutBuilder(builder: (context, constrain) {
+      final itemCount = widget.btnItems.length;
+
+      // without left and right padding
+      final minWidth = buttonSize * itemCount + minGap * (itemCount - 1);
+
+      if (minWidth > constrain.maxWidth) {
+        return SizedBox(
+          height: buttonIconSize,
+          width: double.maxFinite,
+          child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: List<Widget>.generate(itemCount, (index) {
+                if (index != itemCount - 1) {
+                  return Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // RoundButton(
+                      //     icon: widget.btnItems[index].icon,
+                      //     size: buttonIconSize,
+                      //     onPressed: widget.btnItems[index].onPressed),
+                      widget.btnItems[index],
+                      SizedBox(width: minGap)
+                    ],
+                  );
+                } else {
+                  // return RoundButton(
+                  //     icon: widget.btnItems[index].icon,
+                  //     size: buttonIconSize,
+                  //     onPressed: widget.btnItems[index].onPressed);
+                  return widget.btnItems[index];
+                }
+              })),
+        );
+      } else {
+        return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: widget.btnItems);
+      }
+    });
+  }
 }
 
-class AvActionSheetItem {
+class AvActionSheetBtnItem {
   final IconData icon;
   final VoidCallback onPressed;
 
-  AvActionSheetItem({required this.icon, required this.onPressed});
+  AvActionSheetBtnItem({required this.icon, required this.onPressed});
+}
+
+class AvActionSheetListItem {
+  final IconData icon;
+  final String title;
+  final VoidCallback onPressed;
+
+  AvActionSheetListItem(
+      {required this.icon, required this.title, required this.onPressed});
 }
--- lib/ui/agora/av_chat_page.dart
+++ lib/ui/agora/av_chat_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:vocechat_client/dao/init_dao/group_info.dart';
 import 'package:vocechat_client/dao/init_dao/user_info.dart';
 import 'package:vocechat_client/ui/agora/av_action_sheet.dart';
+import 'package:vocechat_client/ui/agora/round_button.dart';
 import 'package:vocechat_client/ui/app_icons_icons.dart';
 import 'package:vocechat_client/ui/app_text_styles.dart';
 import 'package:vocechat_client/ui/widgets/avatar/voce_avatar_size.dart';
@@ -37,14 +38,23 @@ class _AvChatPageState extends State<AvChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [_buildBody(context), AvActionSheet(items: [])],
+        children: [
+          _buildBody(context),
+          AvActionSheet(btnItems: [
+            // Size must be 24, in-button padding must be 16.
+            RoundButton(icon: AppIcons.video, onPressed: () {}, size: 24),
+            RoundButton(icon: AppIcons.video, onPressed: () {}, size: 24),
+            RoundButton(icon: AppIcons.video, onPressed: () {}, size: 24),
+            RoundButton(icon: AppIcons.video, onPressed: () {}, size: 24),
+            RoundButton(icon: AppIcons.video, onPressed: () {}, size: 24),
+          ])
+        ],
       ),
     );
   }
 
   Widget _buildBody(BuildContext context) {
     final topPadding = MediaQuery.of(context).padding.top;
-    final bottomPadding = MediaQuery.of(context).padding.bottom;
 
     return Padding(
       padding: EdgeInsets.only(top: topPadding),
@@ -56,16 +66,19 @@ class _AvChatPageState extends State<AvChatPage> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Row(children: [
-        roundButton(Icons.arrow_back_ios_new, 24, onPressed: () {
-          Navigator.of(context).pop();
-        }),
+        RoundButton(
+            icon: Icons.arrow_back_ios_new,
+            paddingValue: 8,
+            onPressed: () {
+              Navigator.of(context).pop();
+            }),
         Expanded(
           child: Padding(
             padding: const EdgeInsets.only(left: 20, right: 20),
             child: _buildTitleInfo(),
           ),
         ),
-        roundButton(AppIcons.people, 24)
+        RoundButton(icon: AppIcons.people, paddingValue: 8)
       ]),
     );
   }
@@ -103,20 +116,5 @@ class _AvChatPageState extends State<AvChatPage> {
     return Row(
       children: [avatar, SizedBox(width: 8), title],
     );
-  }
-
-  Widget roundButton(IconData icon, double size, {VoidCallback? onPressed}) {
-    return CupertinoButton(
-        padding: EdgeInsets.zero,
-        onPressed: onPressed,
-        child: Container(
-            decoration: BoxDecoration(
-                color: Colors.black.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(size)),
-            child: Center(
-                child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(icon, size: size, color: Colors.white),
-            ))));
   }
 }
