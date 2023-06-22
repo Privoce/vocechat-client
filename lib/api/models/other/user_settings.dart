320dd3d86a090c9958375024869d486cb373ad29
shuotwang <shuotongwang@gmail.com>
2023-06-22 15:48:53 -0700

    fix: rollback image view

---
+++ lib/api/models/other/user_settings.dart
@@ -0,0 +1,15 @@
+// import 'package:json_annotation/json_annotation.dart';
+
+// part 'user_settings.g.dart';
+
+// @JsonSerializable(fieldRename: FieldRename.snake, explicitToJson: true)
+// class UserSettings {
+//   final List<Map<int, String>> reactions;
+
+//   UserSettings(this.reactions);
+
+//   factory UserSettings.fromJson(Map<String, dynamic> json) =>
+//       _$UserSettingsFromJson(json);
+
+//   Map<String, dynamic> toJson() => _$UserSettingsToJson(this);
+// }
