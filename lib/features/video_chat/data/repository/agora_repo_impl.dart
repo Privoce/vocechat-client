d89b765d99df5892680b62140c0e9cb26da6da47
shuotwang <shuotongwang@gmail.com>
2023-11-02 22:44:00 -0700

    add more method definition in repo class; some common app error returns in methods.

--- lib/features/video_chat/data/repository/agora_repo_impl.dart
+++ lib/features/video_chat/data/repository/agora_repo_impl.dart
@@ -10,6 +10,19 @@ class AgoraRepoImpl implements AgoraRepository {
 
   AgoraRepoImpl();
 
+  /// Check if Agora is enabled in the server.
+  ///
+  /// Should be called before any other Agora related methods.
+  @override
+  Future<bool> isAgoraEnabled() async {
+    try {
+      return (await _agoraApis.isAgoraEnabled).data ?? false;
+    } catch (e) {
+      App.logger.info("Agora is not enabled in this server.");
+      return false;
+    }
+  }
+
   /// Get Agora basic info, including token, uid, channel name, etc,
   ///
   /// The retrieved data is used to initialize Agora RTC engine and join channel.
@@ -21,6 +34,15 @@ class AgoraRepoImpl implements AgoraRepository {
   Future<DataState<AgoraBasicInfoModel>> getAgoraBasicInfo(
       {int? uid, int? gid}) async {
     try {
+      if (!(await isAgoraEnabled())) {
+        return InternalError(AppError("Agora is not enabled in this server."));
+      }
+
+      if ((uid == null && gid == null) || (uid != null && gid != null)) {
+        return InternalError(
+            AppError("only one of uid or gid should be provided."));
+      }
+
       final res = await _agoraApis.getAgoraBasicInfo(uid: uid, gid: gid);
       if (res.statusCode == 200 && res.data != null) {
         final data = res.data as AgoraBasicInfoModel;
@@ -34,16 +56,33 @@ class AgoraRepoImpl implements AgoraRepository {
       }
     } on DioException catch (e) {
       return DataFailed(e);
+    } catch (e) {
+      return InternalError(AppError(e.toString()));
     }
   }
 
-  /// Check if Agora is enabled in the server.
-  Future<bool> isAgoraEnabled() async {
-    try {
-      return (await _agoraApis.isAgoraEnabled).data ?? false;
-    } catch (e) {
-      App.logger.info("Agora is not enabled in this server.");
-      return false;
-    }
+  @override
+  void askForAudioPermission() {
+    // TODO: implement askForAudioPermission
+  }
+
+  @override
+  void askForVideoPermission() {
+    // TODO: implement askForVideoPermission
+  }
+
+  @override
+  void initAgoraEngine() {
+    // TODO: implement initAgoraEngine
+  }
+
+  @override
+  void joinChannel() {
+    // TODO: implement joinChannel
+  }
+
+  @override
+  void leaveAndRelease() {
+    // TODO: implement leaveAndRelease
   }
 }
--- lib/features/video_chat/domain/usecases/init_join.dart
+++ lib/features/video_chat/domain/usecases/init_join.dart
@@ -3,14 +3,24 @@ import 'package:vocechat_client/core/usecase/usecase.dart';
 import 'package:vocechat_client/features/video_chat/domain/entities/agora_basic_info.dart';
 import 'package:vocechat_client/features/video_chat/domain/repository/agora_repo.dart';
 
+class AgoraInitJoinParams {
+  int? uid;
+  int? gid;
+
+  AgoraInitJoinParams({this.uid, this.gid}) {
+    assert((uid == null && gid != null) || (uid != null && gid == null));
+  }
+}
+
 class InitJoinAgoraUseCase
-    extends UseCase<DataState<AgoraBasicInfoEntity>, void> {
+    extends UseCase<DataState<AgoraBasicInfoEntity>, AgoraInitJoinParams> {
   final AgoraRepository _repository;
 
   InitJoinAgoraUseCase(this._repository);
 
   @override
-  Future<DataState<AgoraBasicInfoEntity>> call({void params}) {
-    return _repository.getAgoraBasicInfo();
+  Future<DataState<AgoraBasicInfoEntity>> call(
+      AgoraInitJoinParams params) async {
+    return _repository.getAgoraBasicInfo(uid: params.uid, gid: params.gid);
   }
 }
